abbfc27b3cd2927f9a376e8f1c03462b44540229
James Bain <jamescbain@gmail.com>
2016-03-28 23:53:23 -0500

    rename image field to pictures for less confusion

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask import url_for
 from tumblelog import db
 
 class Picture(db.EmbeddedDocument):
-    image = db.ImageField()
+    picture = db.ImageField()
 
 class Comment(db.EmbeddedDocument):
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
@@ -14,7 +14,7 @@ class Post(db.DynamicDocument):
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
     title = db.StringField(max_length=255, required=True)
     slug = db.StringField(max_length=255, required=True)
-    images = db.ListField(db.EmbeddedDocumentField('Picture'))
+    pictures = db.ListField(db.EmbeddedDocumentField('Picture'))
     comments = db.ListField(db.EmbeddedDocumentField('Comment'))
 
     def get_absolute_url(self):
